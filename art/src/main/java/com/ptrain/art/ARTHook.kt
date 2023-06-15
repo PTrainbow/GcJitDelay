bd1fdb17941f03cb2365dc2ebeafc5993a1efd05
PTrainbow <327365600@qq.com>
2023-06-15 17:52:23 +0800

    fix: abi filter

--- art/src/main/java/com/ptrain/art/ARTHook.kt
+++ art/src/main/java/com/ptrain/art/ARTHook.kt
@@ -7,14 +7,17 @@ import com.bytedance.shadowhook.ShadowHook.ConfigBuilder
 object ARTHook {
 
     init {
-        val code =ShadowHook.init(ConfigBuilder().setMode(ShadowHook.Mode.UNIQUE).setDebuggable(true).build())
+
+//        // 此处要考虑架构问题，shadowhook 只支持两种架构，示例工程先不管了
+//        if (!Build.SUPPORTED_ABIS.contains("arm64-v8a") && !Build.SUPPORTED_ABIS.contains("armeabi-v7a")) {
+//            Log.e("ARTHook", "unSupport ABI!")
+//        }
+        ShadowHook.init(
+            ConfigBuilder().setMode(ShadowHook.Mode.UNIQUE).setDebuggable(true).build()
+        )
         System.loadLibrary("art")
     }
 
-    /**
-     * A native method that is implemented by the 'art' native library,
-     * which is packaged with this application.
-     */
     external fun init()
 
 }
